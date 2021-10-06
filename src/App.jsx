fc07e6b19d5c274985ea345fbd69937ce8f9a822
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-10-06 18:27:54 +0300

    third lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
 import { useSelector, useDispatch } from 'react-redux'
 import './App.css'
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer'
 
 function App() {
-const dispatch = useDispatch()
-const cash = useSelector(state => state.cash.cash)
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
-const addCash = (cash) => {
-  dispatch({type:'ADD_CASH', payload: cash})  
-}
-const getCash = (cash) => {
-  dispatch({type:'GET_CASH', payload: cash})  
+  const addCash = (cash) => {
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
+  const getCash = (cash) => {
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
 
-}
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
 
   return (
     <div className="App">
-      <div style={{display:'flex'}}>
-        <button onClick={()=> addCash(Number(prompt()))}>Пополнить счет</button>
-        <button onClick={()=> getCash(Number(prompt()))}>Снять со счета</button>
-        <div style={{fontSize: '3rem'}}>{cash}</div>
+      <div style={{ display: 'flex' }}>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+        <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+        <button onClick={() => getCash(Number(prompt()))}>Удалить клиента</button>
+        <div style={{ fontSize: '3rem' }}>{cash}</div>
       </div>
+      {customers.length > 0 ?
+        <div>
+            {customers.map(customer =>
+              <div onClick={()=>removeCustomer(customer)} style={{fontSize:'2rem', border: '1px solid black', padding: '10px', marginTop:5}}>{customer.name}</div>
+            )}
+        </div>
+        :
+        <div style={{fontSize:'2rem'}}>
+          Клиенты отсутвтвуют!
+        </div>
+      }
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { combineReducers, compose, createStore } from "redux"
+import { combineReducers, createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import {cashReducer} from './cashReducer'
 import {customerReducer} from './customerReducer'
